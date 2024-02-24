f894c04a4f3f2cce4b776a721a42702a4377df51
nagatony <91994@Ramarao>
2024-02-24 11:34:55 +0530

    I have shared even numbers and odd numbers programes.

---
+++ src/ram/Even_nubers.java
@@ -0,0 +1,10 @@
+package ram;
+
+public class Even_nubers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ram/Odd_numbers.java
@@ -0,0 +1,10 @@
+package ram;
+
+public class Odd_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
